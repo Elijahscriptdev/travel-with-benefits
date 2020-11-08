a128d2c22f1172a51cf1ce77adf470bccb887300
Elijahscriptdev <elijahobominuru@gmail.com>
2020-11-08 04:02:14 +0100

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_043314) do
+ActiveRecord::Schema.define(version: 2020_11_08_010948) do
+
+  create_table "travel_infomations", force: :cascade do |t|
+    t.string "title"
+    t.string "travel_from"
+    t.string "travel_to"
+    t.string "travel_date"
+    t.string "bus_type"
+    t.string "departing_time"
+    t.integer "seat_available"
+    t.integer "price"
+    t.string "availability"
+    t.string "company_name"
+    t.string "takeoff_location"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_travel_infomations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,4 +55,5 @@ ActiveRecord::Schema.define(version: 2020_11_07_043314) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "travel_infomations", "users"
 end
